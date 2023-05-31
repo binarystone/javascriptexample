f890d4633223d10d3cebf168eacae7a03cbbf942
Pedro <psinger9000@gmail.com>
2023-05-31 13:24:40 +0800

    modified script.js

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-alert("THIS IS A POPUP")
+// alert("THIS IS A POPUP")
 
+
+// arrays example
+// let numbers = ['one', 'two', 'three'];
+// alert(numbers[]);
+
+// objects example
+let job = {position: 'Sales', place: 'remote', salary:'100000'};
+alert(job[0,0])
